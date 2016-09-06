e54a7aca685720fdcd872387b02a76e8f88bf9c8
martyn megaloudis <martynmme@hotmail.com>
2016-09-06 15:07:13 -0400

    Test

---
+++ Ch1-Ex3/ch1-ex3.py
@@ -0,0 +1,14 @@
+#
+# Template for code submission
+# name : Martyn Megaloudis
+# email : martynmme@hotmail.com
+#date : 9/6/2016
+#class : CS008-f2016
+#instructor : Max Novelli (man8@pitt.edu)
+#
+# Description:
+# Test
+# Example: Starting with Python, Chapter 1, Exercise 3
+#
+#Notes:
+#...and now let's program with python
