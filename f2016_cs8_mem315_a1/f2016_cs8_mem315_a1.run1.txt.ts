7025abf698bcf6e74c7010570adfbe929ce6fe53
martyn megaloudis <martynmme@hotmail.com>
2016-09-30 17:14:50 -0400

    This is the text file that shows two trials for my program. The first is when the user uses the metric system and the second when the user uses the USC system

---
+++ f2016_cs8_mem315_a1/f2016_cs8_mem315_a1.run1.txt.ts
@@ -0,0 +1,29 @@
+Run 1
+/**
+ * Created by martyn on 9/30/2016.
+ */
+C:\Users\martyn\Python\python.exe C:/Users/martyn/Documents/CS0008/CS008-f2016/f2016_cs8_mem315_a1/f2016_cs8_mem315_a1.py
+If you would like to use USC over metric type y if not type n:y
+How many miles did you drive?24561.23
+How much gas in gallons did you use?456.345
+                     	USC       	Metric
+Distance___________: 	24561.230 miles	39527.370 Km
+Gas________________: 	456.345 gallons	1727.453 Liters
+Consumption________: 	53.822 mpg	    4.370 1/100Km
+Gas Consumption Rating:Excellent
+
+Process finished with exit code 0
+
+Run 2
+C:\Users\martyn\Python\python.exe C:/Users/martyn/Documents/CS0008/CS008-f2016/f2016_cs8_mem315_a1/f2016_cs8_mem315_a1.py
+If you would like to use USC over metric type y if not type n:y
+How many miles did you drive?256.75
+How much gas in gallons did you use?30.567
+                     	USC       	Metric
+Distance___________: 	256.750 miles	413.198 Km
+Gas________________: 	30.567 gallons	115.709 Liters
+Consumption________: 	8.400 mpg	    28.003 1/100Km
+Gas Consumption Rating:Extremely poor
+
+Process finished with exit code 0
+
