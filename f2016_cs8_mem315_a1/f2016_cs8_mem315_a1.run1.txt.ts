4898c007b75ea2a0403fd5ba6f13ff530430b3e3
martyn megaloudis <martynmme@hotmail.com>
2016-09-30 18:30:47 -0400

    Run 1 of my program.

--- f2016_cs8_mem315_a1/f2016_cs8_mem315_a1.run1.txt.ts
+++ f2016_cs8_mem315_a1/f2016_cs8_mem315_a1.run1.txt.ts
@@ -1,29 +1,11 @@
-Run 1
-/**
- * Created by martyn on 9/30/2016.
- */
 C:\Users\martyn\Python\python.exe C:/Users/martyn/Documents/CS0008/CS008-f2016/f2016_cs8_mem315_a1/f2016_cs8_mem315_a1.py
 If you would like to use USC over metric type y if not type n:y
-How many miles did you drive?24561.23
-How much gas in gallons did you use?456.345
+How many miles did you drive?12521.678
+How much gas in gallons did you use?345.89
                      	USC       	Metric
-Distance___________: 	24561.230 miles	39527.370 Km
-Gas________________: 	456.345 gallons	1727.453 Liters
-Consumption________: 	53.822 mpg	    4.370 1/100Km
+Distance___________: 	12521.678 miles	20151.637 Km
+Gas________________: 	345.890 gallons	1309.335 Liters
+Consumption________: 	36.201 mpg	    6.497 1/100Km
 Gas Consumption Rating:Excellent
 
 Process finished with exit code 0
-
-Run 2
-C:\Users\martyn\Python\python.exe C:/Users/martyn/Documents/CS0008/CS008-f2016/f2016_cs8_mem315_a1/f2016_cs8_mem315_a1.py
-If you would like to use USC over metric type y if not type n:y
-How many miles did you drive?256.75
-How much gas in gallons did you use?30.567
-                     	USC       	Metric
-Distance___________: 	256.750 miles	413.198 Km
-Gas________________: 	30.567 gallons	115.709 Liters
-Consumption________: 	8.400 mpg	    28.003 1/100Km
-Gas Consumption Rating:Extremely poor
-
-Process finished with exit code 0
-
