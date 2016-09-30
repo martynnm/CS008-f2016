7cdc93c29fd04f1ba49c8aafaed2027a71b2bc18
martyn megaloudis <martynmme@hotmail.com>
2016-09-29 23:56:59 -0400

    Testing code for assingment

--- f2016_cs8_mem315_a1/f2016_cs8_mem315_a1.py
+++ f2016_cs8_mem315_a1/f2016_cs8_mem315_a1.py
@@ -1,6 +1,6 @@
 # First we must ask whether the user wants to use the USC or Metric system
 system = input("If you would like to use USC over metric type y if not type n:")
-# Now that we have what system to use we will find their distance and gas usage
+# Now that we have the system to use we will find their distance and gas usage
 if system == 'y':
     distance = ((float(input("How many miles did you drive?"))))
     fuel = ((float(input("How much gas in gallons did you use?"))))
